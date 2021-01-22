692e55986a2922067fd3aa708dd82be04b94ab44
Nikos Kechris <74186791+nkechris@users.noreply.github.com>
2021-01-22 17:31:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
-window.saveDataAcrossSessions = true;
+window.saveDataAcrossSessions = true
 
-const LOOK_DELAY = 1000;
-const LEFT_CUTOFF = window.innerWidth / 4;
-const RIGHT_CUTOFF = window.innerWidth - window.innerWidth / 4;
+const LOOK_DELAY = 1000
+const LEFT_CUTOFF = window.innerWidth / 4
+const RIGHT_CUTOFF = window.innerWidth - window.innerWidth / 4
 
+let startLookTime = Number.POSITIVE_INFINITY
+let lookDirection = null
+let imageElement = getNewImage()
+let nextImageElement = getNewImage(true)
 
-let startLookTime = Number.POSITIVE_INFINITY;
-let imageElement = getNewImage();
-let nextImageElement = getNewImage(true);
+webgazer.setGazeListener((data, timestamp) => {
+    if (data == null || lookDirection === "STOP") return
 
-
-webgazer.SetGazeListener((data, timestap)=>{
-    if (data == null || lookDirection === 'STOP') return;
-
-    if (data.x < LEFT_CUTOFF && lookDirection !== 'LEFT' && lookDirection !=='RESET') {
-        startLookTime = timestap;
-        lookDirection = 'LEFT';
-    } else if (data.x > RIGHT_CUTOFF && lookDirection !== 'RIGHT' && lookDirection !=='RESET') {
-        startLookTime = timestap;
-        lookDirection = 'RIGHT';
+    if (
+      data.x < LEFT_CUTOFF &&
+      lookDirection !== "LEFT" &&
+      lookDirection !== "RESET"
+    ) {
+      startLookTime = timestamp
+      lookDirection = "LEFT"
+    } else if (
+      data.x > RIGHT_CUTOFF &&
+      lookDirection !== "RIGHT" &&
+      lookDirection !== "RESET"
+    ) {
+      startLookTime = timestamp
+      lookDirection = "RIGHT"
     } else if (data.x >= LEFT_CUTOFF && data.x <= RIGHT_CUTOFF) {
-        startLookTime = Number.POSITIVE_INFINITY;
-        lookDirection = null;
-
-
+      startLookTime = Number.POSITIVE_INFINITY
+      lookDirection = null
     }
-    if (startLookTime + LOOK_DELAY < timestap) {
-        if (lookDirection === 'LEFT'){
-            imageElement.classList.add("left");
-        }else{
-            imageElement.classList.add("right");
-        }
 
-        startLookTime = Number.POSITIVE_INFINITY;
-        lookDirection = 'STOP';
-        setTimeout(()=>{
-            imageElement.remove();
-            imageElement = nextImageElement;
-            nextImageElement = getNewImage(true);
-            lookDirection = "RESET";
-        }, 200)
+    if (startLookTime + LOOK_DELAY < timestamp) {
+      if (lookDirection === "LEFT") {
+        imageElement.classList.add("left")
+      } else {
+        imageElement.classList.add("right")
+      }
+
+      startLookTime = Number.POSITIVE_INFINITY
+      lookDirection = "STOP"
+      setTimeout(() => {
+        imageElement.remove()
+        nextImageElement.classList.remove("next")
+        imageElement = nextImageElement
+        nextImageElement = getNewImage(true)
+        lookDirection = "RESET"
+      }, 200)
     }
-})
-.begin()
+  })
+  .begin()
 
-webgazer.ShowVideoPreview(false).showPredictionPoints(false);
+webgazer.showVideoPreview(false).showPredictionPoints(false)
 
-function getNewImage(next = false){
-    const img = document.createElement("img");
-    img.src ="https://picsum.photos/1000?" + Math.random();
-    if (next) img.classList.add("next")
-    document.body.append(img);
-    return img;
+function getNewImage(next = false) {
+  const img = document.createElement("img")
+  img.src = "https://picsum.photos/1000?" + Math.random()
+  if (next) img.classList.add("next")
+  document.body.append(img)
+  return img
 }
